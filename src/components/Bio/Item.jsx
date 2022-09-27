bd514815f343138f18cbb52858730d0ea22363a4
fandiarfa26 <arfabuma.987@gmail.com>
2022-09-27 21:06:33 +0700

    fix bio

--- src/components/Bio/Item.jsx
+++ src/components/Bio/Item.jsx
@@ -6,8 +6,11 @@ const BioItem = (props) => {
     <div className='flex flex-col items-center w-full gap-3'>
       
       <img data-aos="flip-right" data-aos-duration="1500" src={props.pic} alt={props.name} className='mb-5 border-4 border-white shadow-xl h-36 lg:border-8 lg:h-48' />
-      <div data-aos="fade-up" className='mb-3 text-2xl text-center lg:text-3xl font-handwriting'>{props.name}</div>
-      
+      <div data-aos="fade-up" className='text-2xl text-center lg:text-3xl font-handwriting'>{props.name}</div>
+      <div>
+        <div className='text-sm text-center'>{props.childof}</div>
+        <div className='text-sm text-center'>{props.parent}</div>
+      </div>
       
       <a data-aos="fade-up" href={`https://www.instagram.com/${props.instagram}/`} target="_blank" rel='noreferrer' className='flex items-center gap-2 px-4 py-2 text-sm border-2 border-coklat-dark hover:border-ijo-dark hover:bg-ijo-light'>
         <FaInstagram className='w-5 h-5'/>
--- src/components/Bio/index.jsx
+++ src/components/Bio/index.jsx
@@ -5,6 +5,20 @@ import fandiPhoto from '../../assets/images/fandi.jpg'
 import BioItem from './Item'
 
 const Bio = ({greet, from}) => {
+  const data = [
+    {
+      name: 'M. Fandi Arfabuma, S.T.',
+      childof: 'Putra pertama dari',
+      parent: 'Bapak Suhardi & Ibu Latifah',
+      instagram: 'fandiarfa',
+    },
+    {
+      name: 'Desy Rosmita Maharani, S.Pd.',
+      childof: 'Putri ketiga dari',
+      parent: 'Bapak Maharani Kahar & Ibu St. Rumiyati',
+      instagram: 'dsyrsmitam',
+    },
+  ];
   return (
     <div id="bio" className='px-5 pb-10 lg:px-20'>
       <div className="flex flex-col items-center gap-5">
@@ -14,13 +28,17 @@ const Bio = ({greet, from}) => {
         <div className="flex flex-col items-start w-full gap-5 py-5 lg:flex-row">
           <BioItem 
             pic={from === 'Fandi' ? fandiPhoto : mithaPhoto}
-            name={from === 'Fandi' ? "M. Fandi Arfabuma, S.T.": "Desy Rosmita Maharani, S.Pd."}
-            instagram={from === 'Fandi' ? "fandiarfa" : "dsyrsmitam"}/>
+            name={from === 'Fandi' ? data[0].name : data[1].name}
+            childof={from === 'Fandi' ? data[0].childof : data[1].childof}
+            parent={from === 'Fandi' ? data[0].parent : data[1].parent}
+            instagram={from === 'Fandi' ? data[0].instagram : data[1].instagram}/>
           <img src={flower} alt="Flower" className='self-center h-28 lg:h-32' />
           <BioItem 
             pic={from === 'Mitha' ? fandiPhoto : mithaPhoto}
-            name={from === 'Mitha' ? "M. Fandi Arfabuma, S.T.": "Desy Rosmita Maharani, S.Pd."}
-            instagram={from === 'Mitha' ? "fandiarfa" : "dsyrsmitam"}/>
+            name={from === 'Mitha' ? data[0].name : data[1].name}
+            childof={from === 'Mitha' ? data[0].childof : data[1].childof}
+            parent={from === 'Mitha' ? data[0].parent : data[1].parent}
+            instagram={from === 'Mitha' ? data[0].instagram : data[1].instagram}/>
         </div>
       </div>
     </div>
